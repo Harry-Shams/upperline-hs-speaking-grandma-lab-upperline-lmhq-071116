7bc7eaa7c2ad98248486d1ee30d1b7d206c5c0f4
Harry Shams <harathon@gmail.com>
2016-07-12 18:22:28 +0000

    i beat david adler

--- lib/grandma.rb
+++ lib/grandma.rb
@@ -1,2 +1,9 @@
-# Write your code here!
+def speak_to_grandma(statement)
+  if statement == statement.upcase
+    puts "No, not since 1938"
+  else
+        puts "HUH?! SPEAK UP SUNNY"
+  end
+end
 
+speak_to_grandma("kjkhl")
