5d99144197b29dc8c4fd5a45215164920b460883
Harry Shams <harathon@gmail.com>
2016-07-12 19:48:47 +0000

    done.

--- lib/grandma.rb
+++ lib/grandma.rb
@@ -1,9 +1,29 @@
 def speak_to_grandma(statement)
-  if statement == statement.upcase
-    puts "No, not since 1938"
-  else
+  while statement != "BYE"
+    if statement == statement.upcase
+      puts "No, not since 19#{1 + rand(3...5)}#{1 + rand(9)}"
+    else
         puts "HUH?! SPEAK UP SUNNY"
+    end
+  statement = gets.chomp
+  if statement != "BYE" && statement == statement.upcase
+    puts "No, not since 19#{1 + rand(3...5)}#{1 + rand(9)}"
+  else
+      puts "HUH?! SPEAK UP SUNNY"
+  end
+  statement = gets.chomp
+  if statement != "BYE" && statement == statement.upcase
+    puts "No, not since 19#{1 + rand(3...5)}#{1 + rand(9)}"
+  else
+      puts "HUH?! SPEAK UP SUNNY"
+  end
+  statement = gets.chomp
+  if statement != "BYE" && statement == statement.upcase
+    puts "No, not since 19#{1 + rand(3...5)}#{1 + rand(9)}"
+  else
+      puts "HUH?! SPEAK UP SUNNY"
+  end
   end
 end
 
-speak_to_grandma("kjkhl")
+puts speak_to_grandma("hi")
